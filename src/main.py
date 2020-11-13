b8cb1dd4d25c4d068d21a221cf76b1bcbbc6dc65
Przemysław Buczkowski <przemub@przemub.pl>
2020-11-13 01:49:42 +0000

    Start!

---
+++ src/main.py
@@ -0,0 +1,43 @@
+import gettext
+from typing import List
+
+from pywikibot import Category, Site, Page
+from pywikibot.pagegenerators import CategorizedPageGenerator
+
+trans = gettext.translation('base', localedir='locales')
+_ = trans.gettext
+
+
+def clean(category: Category, supercategories: List[Category]):
+    print(_("Cleaning category {0}").format(category.title()))
+    for page in CategorizedPageGenerator(category):
+        page: Page
+
+        for page_category in page.categories():
+            if page_category in supercategories:
+                print(_("{1}: Removing {0}, a supercategory of {1}").format(
+                    page.title(), page_category.title(), category.title()
+                ))
+                page.change_category(
+                    page_category,
+                    None,
+                    _("Removed {0} since it is a supercategory of {1}").format(
+                        f"[[{page_category.title()}]]",
+                        f"[[{category.title()}]]"
+                    )
+                )
+
+    for subcategory in category.subcategories():
+        clean(subcategory, supercategories + [category])
+
+
+def main():
+    site = Site()
+
+    category_name = input(_("Category to clear: "))
+    category = Category(site, category_name)
+    clean(category, [])
+
+
+if __name__ == '__main__':
+    main()
